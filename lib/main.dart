9b44af3c3f20546bc2290f7a8d19d965b1f074a8
Babar78 <muhammadbabarashraf8@gmail.com>
2024-03-07 22:59:12 +0500

    Task 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:practice_class/stopwatchcomponent.dart';
+import './stopwatch.dart';
 
-void main() {
-  runApp(const StopwatchApp());
-}
+void main() => runApp(StopwatchApp());
 
 class StopwatchApp extends StatelessWidget {
-  const StopwatchApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const StopWatchComponent(),
+      home: StopWatch(),
     );
   }
 }
---
+++ lib/stopwatch.dart
@@ -0,0 +1,140 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class StopWatch extends StatefulWidget {
+  @override
+  State createState() => StopWatchState();
+}
+
+class StopWatchState extends State<StopWatch> {
+  int milliseconds = 0;
+  Timer? timer;
+  bool isTicking = true;
+  final laps = <int>[];
+
+  void _onTick(Timer time) {
+    setState(() {
+      milliseconds += 1000;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stopwatch'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: _buildCounter(context)),
+          Expanded(child: _buildLapDisplay()),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCounter(BuildContext context) {
+    return Container(
+      color: Theme.of(context).primaryColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Lap ${laps.length + 1}',
+            style: Theme.of(context)
+                .textTheme
+                .subtitle1
+                ?.copyWith(color: Colors.white),
+          ),
+          Text(
+            _secondsText(milliseconds),
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                ?.copyWith(color: Colors.white),
+          ),
+          const SizedBox(height: 20),
+          _buildControls()
+        ],
+      ),
+    );
+  }
+
+  Widget _buildControls() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          ),
+          child: Text('Start'),
+          onPressed: isTicking ? null : _startTimer,
+        ),
+        const SizedBox(width: 20),
+        ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow),
+          ),
+          onPressed: isTicking ? _lap : null,
+          child: const Text('Lap'),
+        ),
+        const SizedBox(width: 20),
+        TextButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          ),
+          child: Text('Stop'),
+          onPressed: isTicking ? _stopTimer : null,
+        )
+      ],
+    );
+  }
+
+  void _startTimer() {
+    timer = Timer.periodic(const Duration(milliseconds: 100), _onTick);
+    setState(() {
+      milliseconds = 0;
+      isTicking = true;
+      laps.clear();
+    });
+  }
+
+  void _lap() {
+    setState(() {
+      laps.add(milliseconds);
+      milliseconds = 0;
+    });
+  }
+
+  void _stopTimer() {
+    timer?.cancel();
+    setState(() {
+      isTicking = false;
+    });
+  }
+
+  Widget _buildLapDisplay() {
+    return ListView(
+      children: [
+        for (int milliseconds in laps)
+          ListTile(
+            title: Text(_secondsText(milliseconds)),
+          )
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  String _secondsText(int milliseconds) {
+    final seconds = milliseconds / 1000;
+    return '$seconds seconds';
+  }
+}
